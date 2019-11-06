822b9bc10240190a190a0d46f4767c1d9fc77176
DaleGranger123 <56914050+DaleGranger123@users.noreply.github.com>
2019-11-06 00:14:53 +0000

    Add files via upload

--- SEProject.py
+++ SEProject.py
@@ -1,12 +1,13 @@
 from newsapi.newsapi_client import NewsApiClient
-import pandas as pd
 import requests
 import spotipy
-#import spotipy.outh2 as oauth2
-import sys
+import spotipy.oauth2 as oauth2
+import random
+import nltk
+from nltk.corpus import words
 
 
-
+## Retrieve top headlines and append to list.
 def TopHeadlines():
     news_URL = "https://newsapi.org/v2/top-headlines?country=gb&apiKey=108a2d7663e94e639f96a3a6c77d82f1"
     open_page = requests.get(news_URL).json()
@@ -16,32 +17,41 @@ def TopHeadlines():
         headlines.append(i["title"])
     for j in range (len(headlines)):
         return headlines
-    data = TopHeadlines()
-    print (data)
     
 Headlines = TopHeadlines()
 
+## if word in list is longer than 5 characters
+## then append to data
+Headlines1 = "".join(Headlines)
+processedData = ' '.join([w for w in Headlines1.split() if len(w)>5])
+data = processedData.split()
 
-print (Headlines)
+## Download word dictionary
+nltk.download('words')
+## Check if word is in english dictionary and append to new list
+newdata = []
+for i in data:
+    if i in words.words():
+        newdata.append(i)
+
+## Choose random word from list
+Random_News_Word = random.choice(newdata)
+print (Random_News_Word)
 
 
-#credentials = oauth2.SpotifyClientCredentials(
-#        client_id="c367030cba0547c18102ec491320e635",
-#        client_secret="3286485fbcca41f7b324a6602221755e")
+## Spotify Authentication
+credentials = oauth2.SpotifyClientCredentials(
+        client_id="c367030cba0547c18102ec491320e635",
+        client_secret="3286485fbcca41f7b324a6602221755e")
 
-#token = credentials.get_access_token()
-#spotify = spotipy.Spotify(auth=token)
+token = credentials.get_access_token()
+sp = spotipy.Spotify(auth=token)
 
 
-#spotify = spotipy.Spotify()
 
-#if len(sys.argv) > 1:
-#    name = ' '.join(sys.argv[1:])
-#else:
-#    name = 'Space*'
+## Search spotify for keyword extracted from headlines
+##returns 20 tracks that are based on the 'Random_News_Word'
+results = sp.search(q=Random_News_Word, limit=20)
+for i, t in enumerate(results['tracks']['items']):
+    print (' ', i, t['name'])
 
-#results = spotify.search(q='artist:' + name, type='artist')
-#items = results['artists']['items']
-#if len(items) > 0:
-#    artist = items[0]
-#    print (artist['name'], artist['images'][0]['url'])
